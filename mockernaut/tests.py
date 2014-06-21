c4a29bf0fa945d8bdedab9b6e2b4406b467e8685
Sergey Orlov <s.orlov@cair.ru>
2014-06-22 03:24:28 +0400

    Compat for response data loads in tests.

---
+++ mockernaut/tests.py
@@ -0,0 +1,26 @@
+
+import sys
+import json
+
+if sys.version_info[:2] < (2, 7):  # pragma: no cover
+    import unittest2 as unittest
+else:
+    import unittest
+
+
+from mockernaut.server import create_app
+from mockernaut.compat import text_type
+
+
+class ApiTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.app.config['TESTING'] = True
+
+        self.client = self.app.test_client()
+
+        self.app.storage.delete_all()
+
+
+def loads(data):
+    return json.loads(text_type(data))
