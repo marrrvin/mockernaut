a3b6cdb48d7d28bca064d6e309226b22f2d15d56
Sergey Orlov <sergey.orlov@corp.mail.ru>
2014-06-24 11:42:32 +0400

    Do not repeat yourself.

--- setup.py
+++ setup.py
@@ -5,8 +5,16 @@ from setuptools import find_packages
 from mockernaut import __version__
 
 
+with open('README.md') as fp:
+    description = fp.readline()
+
+with open('requirements.txt') as fp:
+    requirements = [req.strip() for req in fp.readlines() if not req.startswith('--')]
+
+
 setup(
     name='mockernaut',
+    description=description,
     version=__version__,
     url='http://github.com/marrrvin/mockernaut/',
     author='Sergey Orlov',
@@ -16,12 +24,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     platforms='any',
-    install_requires=[
-        'Flask',
-        'mysql-connector-python',
-        'requests',
-        'jsonschema'
-    ],
+    install_requires=requirements,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
