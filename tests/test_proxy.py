7cbc8ee7e2258abf0049c44d64c2b16e993ffb36
Sergey Orlov <foobar@list.ru>
2014-07-02 02:10:01 +0400

    Basic test for proxy.

---
+++ tests/test_proxy.py
@@ -0,0 +1,26 @@
+
+from mockernaut.tests import TestCase
+
+
+class ApiTestCase(TestCase):
+    def test_one_rule(self):
+        status_code = 404
+        body = u'Not found'
+        content_type = u'text/plain'
+
+        self.storage.create({
+            u'request': {
+                u'path': u'/',
+            },
+            u'response': {
+                u'status': status_code,
+                u'headers': [[u'Content-type', content_type]],
+                u'body': body
+            },
+        })
+
+        resp = self.client.get('/')
+
+        self.assertEqual(status_code, resp.status_code)
+        self.assertEqual(content_type, resp.content_type)
+        self.assertEqual(body, resp.data)
