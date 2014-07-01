ee4563a81631dea1ef93268af00857fb6989887f
Sergey Orlov <foobar@list.ru>
2014-07-02 03:23:32 +0400

    Do not force text type.

--- mockernaut/views/decorators.py
+++ mockernaut/views/decorators.py
@@ -6,14 +6,12 @@ from flask import g
 from flask.json import loads
 from jsonschema import validate
 
-from ..compat import text_type
-
 
 def schema(schema_doc):
     def decorator(func):
         @wraps(func)
         def decorated_function(*args, **kwargs):
-            doc = loads(text_type(request.data))
+            doc = loads(request.data)
             validate(doc, schema_doc)
             g.doc = doc
 
