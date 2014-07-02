391d2e7a49fe4e88769c0b6e5d11ec3c25e90834
Sergey Orlov <sergey.orlov@corp.mail.ru>
2014-07-02 14:58:07 +0400

    Support for match by request method.

--- mockernaut/views/proxy.py
+++ mockernaut/views/proxy.py
@@ -1,4 +1,7 @@
 
+import operator
+from collections import defaultdict
+
 from flask import Blueprint
 from flask import request
 from flask import Response
@@ -7,13 +10,24 @@ from flask import current_app as app
 proxy = Blueprint('proxy', __name__)
 
 
-def find_by_request(request):
-    rules = app.storage.get_by_path(request.path)
+def find_by_request(req):
+    score = {}
+    rules = app.storage.get_by_path(req.path)
 
-    return rules[0]
+    for number, rule in enumerate(rules):
+        score[number] = 0
+        rule_req = rule['request']
+        if request.method in rule_req.get('methods', []):
+            score[number] += 1
+
+    winner_number, best_score = max(
+        score.iteritems(), key=operator.itemgetter(1)
+    )
+
+    return rules[winner_number]
 
 
-@proxy.route('')
+@proxy.route('', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'])
 def main():
     rule = find_by_request(request)
     response = rule['response']
--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -3,7 +3,7 @@ from mockernaut.tests import TestCase
 
 
 class ApiTestCase(TestCase):
-    def test_one_rule(self):
+    def test_match_single_rule(self):
         status_code = 404
         content_type = u'text/plain'
 
@@ -23,3 +23,35 @@ class ApiTestCase(TestCase):
         self.assertEqual(status_code, response.status_code)
         self.assertEqual(content_type, response.content_type)
         self.assertEqual(b'Not found', response.data)
+
+    def test_match_same_path_different_methods(self):
+        content_type = u'text/plain'
+
+        self.storage.create({
+            u'request': {
+                u'path': u'/',
+                u'methods': ['GET']
+            },
+            u'response': {
+                u'status': 405,
+                u'headers': [[u'Content-type', content_type]],
+                u'body': u'GET'
+            },
+        })
+        self.storage.create({
+            u'request': {
+                u'path': u'/',
+                u'methods': ['POST']
+            },
+            u'response': {
+                u'status': 200,
+                u'headers': [[u'Content-type', content_type]],
+                u'body': u'POST'
+            },
+        })
+
+        response = self.client.post('/')
+
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(content_type, response.content_type)
+        self.assertEqual(b'POST', response.data)
